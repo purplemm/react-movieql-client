2224dbb9146a69e4f894ecf5eb50314a3a5fcb1e
Boram No <boramm316@gmail.com>
2022-06-14 15:27:00 +0900

    Add pages js

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,6 +1,69 @@
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIE = gql`
+    query ($movieId: String!) {
+        movie(id: $movieId) {
+            id
+            title
+            medium_cover_image
+            rating
+        }
+    }
+`
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    color: white;
+`
+
+const Column = styled.div`
+    width: 50%;
+    margin-left: 10px;
+`
+
+const Title = styled.h1`
+    margin-bottom: 15px;
+    font-size: 65px;
+`
+
+const Subtitle = styled.h4`
+    margin-bottom: 10px;
+    font-size: 35px;
+`
+
+const Image = styled.div`
+    width: 25%;
+    height: 60%;
+    border-radius: 7px;
+    background-color: transparent;
+    background-image: url(${(props) => props.bg});
+    background-position: center center;
+    background-size: cover;
+`
+
 function MovieDetail(){
+    const { id } = useParams();
+    const { data, loading } = new useQuery(GET_MOVIE, {
+        variables: {
+            movieId: id
+        }
+    });
+
     return (
-        <div>movie detail</div>
+        <Container>
+            <Column>
+                <Title>{loading ? "Loading..." : `${data.movie.title}`}</Title>
+                <Subtitle>⭐️ {data && data.movie.rating}</Subtitle>
+            </Column>
+            <Image bg={data && data.movie.medium_cover_image} />
+        </Container>
     )
 }
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,97 @@
+import styled from "styled-components";
+import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+
+const ALL_MOVIES = gql`
+    {
+        allMovies {
+            id
+            title
+            medium_cover_image
+        }
+    }
+`
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`
+
+const Header = styled.header`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 45vh;
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    color: white;
+`
+
+const Title = styled.h1`
+    margin-bottom: 20px;
+    font-size: 60px;
+    font-weight: 600;
+`
+
+const Loading = styled.div`
+    margin-top: 10px;
+    font-size: 18px;
+    font-weight: 500;
+    opacity: 0.5;
+`
+
+const MoviesGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 25px;
+    position: relative;
+    top: -50px;
+    width: 60%;
+`
+
+const PosterContainer = styled.div`
+    width: 100%;
+    height: 400px;
+    border-radius: 7px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    background-color: transparent;
+`
+
+const PosterBg = styled.div`
+    width: 100%;
+    height: 100%;
+    border-radius: 7px;
+    background-image: url(${(props) => props.background});
+    background-position: center center;
+    background-size: cover;
+`
+
 function MovieList(){
+    const { data, loading } = useQuery(ALL_MOVIES);
+
     return (
-        <div>movie list</div>
-    )
+        <Container>
+            <Header>
+                <Title>Movie List</Title>
+            </Header>
+
+            {/* JS에서 boolean && expression은 true인 경우 expression이 출력되고, false인 경우 무시 */}
+            {loading && <Loading>Loading...</Loading>}  
+
+            <MoviesGrid>
+                {data && data.allMovies.map((movie) => (
+                    <PosterContainer key={movie.id}>
+                        <Link to={`/movies/${movie.id}`}>
+                            <PosterBg background={movie.medium_cover_image} />
+                        </Link>
+                    </PosterContainer>
+                ))}
+            </MoviesGrid>
+        </Container>
+    );
 }
 
 export default MovieList;
